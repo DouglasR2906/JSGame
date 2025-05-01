f0627f163620ac444f58308cddd5c7fad31c9085
Douglas R. Oliveira <62523815+DouglasR2906@users.noreply.github.com>
2025-05-01 17:02:34 -0300

    feat: adicionar estrutura HTML inicial e estilo para a interface do jogo em JS
    - Criar index.html com layout básico, incluindo título, campo de entrada e botões.
    - Implementar design responsivo com CSS em style.css, incluindo fundo gradiente e estilo de contêiner.
    - Adicionar consultas de mídia para melhor responsividade em dispositivos móveis.
    - Incluir fontes externas e um script para funcionalidade de voz.
    - Included external fonts and a script for voice functionality.

---
+++ app.js
@@ -0,0 +1,90 @@
+let tentativas = 0;
+let listaDeNumerosSorteados = [];
+const range = 50;
+let possibilidades = 10;
+let numeroAletorio = gerarNumeroAleatorio();
+iniciar();
+
+function gerarNumeroAleatorio() {
+  console.log(listaDeNumerosSorteados);
+  let numeroSorteado = parseInt(Math.random() * range + 1);
+  if (listaDeNumerosSorteados.length == possibilidades) {
+    listaDeNumerosSorteados = [];
+  }
+  if (listaDeNumerosSorteados.includes(numeroSorteado)) {
+    gerarNumeroAleatorio();
+  } else {
+    listaDeNumerosSorteados.push(numeroSorteado);
+    return numeroSorteado;
+  }
+}
+
+function iniciar() {
+  exibirTexto("h1", "Jogo do número secreto!");
+  exibirTexto("p", `Escolha um número entre 1 e ${range}!`);
+  tentativas = 1;
+  focaCampo();
+}
+
+function exibirTexto(tag, texto) {
+  let elemento = document.querySelector(tag);
+  elemento.innerHTML = texto;
+  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
+}
+
+function focaCampo() {
+  let chute = document.querySelector("input");
+  chute.value = "";
+  chute.placeholder = "Digite seu chute aqui!";
+  chute.focus();
+}
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+
+  if (chute == "") {
+    exibirTexto("p", "Você não digitou nada! Tente novamente!");
+    focaCampo();
+    return;
+  }
+
+  if (chute < 1 || chute > range) {
+    exibirTexto(
+      "p",
+      `O número deve estar entre 1 e ${range}. Tente novamente!`
+    );
+    focaCampo();
+    return;
+  }
+
+  if (numeroAletorio == chute) {
+    let mensagem = `Você acertou após ${tentativas} ${
+      tentativas > 1 ? "tentativas" : "tentativa"
+    }! O número era ${numeroAletorio}`;
+    exibirTexto("h1", mensagem);
+    exibirTexto("p", "Clique em 'Novo Jogo' para jogar novamente!");
+    document.getElementById("reiniciar").disabled = false;
+  } else {
+    if (numeroAletorio > chute) {
+      exibirTexto(
+        "p",
+        "O número secreto é maior que " + chute + " tente novamente!"
+      );
+      tentativas++;
+      focaCampo();
+    } else {
+      exibirTexto(
+        "p",
+        "O número secreto é menor que " + chute + " tente novamente!"
+      );
+      tentativas++;
+      focaCampo();
+    }
+  }
+}
+
+function novoJogo() {
+  numeroAletorio = gerarNumeroAleatorio();
+  iniciar();
+  document.getElementById("reiniciar").disabled = true;
+}
